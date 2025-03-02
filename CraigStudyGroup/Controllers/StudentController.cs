87f7dc1d00cbd8e793ee73528318ecce350b92c9
u22635930 <u22635930@tuks.co.za>
2025-03-02 18:52:57 +0200

    Final cleanup: Removed unnecessary files and updated .gitignore

---
+++ CraigStudyGroup/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CraigStudyGroup.Models;
+using System.Collections.Generic;
+
+namespace CraigStudyGroup.Controllers
+{
+    public class StudentController : Controller
+    {
+        // Static list to hold student data (no database needed)
+        private static List<Student> students = new List<Student>
+        {
+            new Student("u01234567", "Craig", "Hasenjager", "u22635930@tuks.co.za"),
+            new Student("u12345678", "Steyn", "Coetzee", "12345678@tuks.co.za"),
+            new Student("u23456789", "Alice", "Steyn", "23456789@tuks.co.za"),
+            new Student("u34567890", "Bob", "Dylan", "34567890@tuks.co.za"),
+            new Student("u45678901", "Eve", "Adam", "45678901@tuks.co.za")
+        };
+
+        public IActionResult Index()
+        {
+            return View(students);
+        }
+    }
+}
---
+++ CraigStudyGroup/Models/student.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CraigStudyGroup.Models
+{
+    public class Student
+    {
+        [Required]
+        [StringLength(10, MinimumLength = 8)]
+        public string StudentNumber { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        public string Surname { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        // Constructor
+        public Student(string studentNumber, string name, string surname, string email)
+        {
+            StudentNumber = studentNumber;
+            Name = name;
+            Surname = surname;
+            Email = email;
+        }
+
+        // Default constructor (needed for MVC model binding)
+        public Student() { }
+    }
+}
+
